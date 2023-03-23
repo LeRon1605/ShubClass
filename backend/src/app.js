b3af61f3def8d09d55a8f41d74625960eaa7540d
Ron Le <ronle9519@gmail.com>
2023-03-23 10:12:43 +0700

    feat: add swagger definition

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(Logger);
+// app.use(Logger);
 app.use('/api-docs', Swagger());
 app.use('/api', routers);
 app.use(ExceptionMiddleware);
