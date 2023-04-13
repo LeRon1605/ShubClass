a3574c5655bcdc49a2b315405d086d9a1656800d
Quytrw <trankimquy1952@gmail.com>
2023-04-13 13:13:17 +0700

    class detail

--- mobile/app/src/main/java/com/androidexam/shubclassroom/api/RetrofitClient.java
+++ mobile/app/src/main/java/com/androidexam/shubclassroom/api/RetrofitClient.java
@@ -9,7 +9,7 @@ public class RetrofitClient {
     public static Retrofit getRetrofitInstance() {
         if(retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://139.162.62.215/api/")
+                    .baseUrl("http://localhost:80/api/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
