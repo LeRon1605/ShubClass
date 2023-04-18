f4fe241e3b09caafa3dcf449424a56ea93f35004
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2023-04-18 14:06:37 +0700

    feat: search class by id

--- backend/src/api/class/class.controller.js
+++ backend/src/api/class/class.controller.js
@@ -1,52 +1,67 @@
-import { ForbiddenException } from '../../exceptions/index.js';
-import ClassService from './class.service.js';
-import { ClassCreateDto, ClassUpdateDto } from './dtos/index.js';
+import { ForbiddenException } from "../../exceptions/index.js";
+import ClassService from "./class.service.js";
+import { ClassCreateDto, ClassUpdateDto } from "./dtos/index.js";
 class ClassController {
-    async getAllClasses(req, res, next) {
-        try {
-            if (req.session.role == 'Student') {
-                data = await ClassService.getAllClassesOfStudent(req.session.id);
-            } else {
-                data = await ClassService.getAllClassesOfTeacher(req.session.id);
-            }
-            return res.status(200).json(data);
-        } catch (error) {
-            next(error);
-        }
+  async getAllClasses(req, res, next) {
+    try {
+      if (req.session.role == "Student") {
+        data = await ClassService.getAllClassesOfStudent(req.session.id);
+      } else {
+        data = await ClassService.getAllClassesOfTeacher(req.session.id);
+      }
+      return res.status(200).json(data);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async createClass(req, res, next) {
-        try {
-            if (req.session.role == 'Teacher') {
-                const entity = ClassCreateDto.toEntity({ ...req.body, teacherId: req.session.id });
-                const result = await ClassService.createClass(entity);
-                return res.status(201).json(result);
-            } else {
-                throw new ForbiddenException('You are not allowed to create class.');
-            }
-        } catch (error) {
-            next(error);
-        }
+  async searchClassById(req, res, next) {
+    try {
+      data = await ClassService.getAllClassesById(req.session.id);
+      return res.status(200).json(data);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async updateClass(req, res, next) {
-        try {
-            const entity = ClassUpdateDto.toEntity({ ...req.body, teacherId: req.session.id });
-            const result = await ClassService.updateClass(req.params.id, entity);
-            return res.status(200).json(result);
-        } catch (error) {
-            next(error);
-        }
+  async createClass(req, res, next) {
+    try {
+      if (req.session.role == "Teacher") {
+        const entity = ClassCreateDto.toEntity({
+          ...req.body,
+          teacherId: req.session.id,
+        });
+        const result = await ClassService.createClass(entity);
+        return res.status(201).json(result);
+      } else {
+        throw new ForbiddenException("You are not allowed to create class.");
+      }
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async deleteClass(req, res, next) {
-        try {
-            await ClassService.deleteClass(req.params.id, req.session.id);
-            return res.status(200).json();
-        } catch (error) {
-            next(error);
-        }
+  async updateClass(req, res, next) {
+    try {
+      const entity = ClassUpdateDto.toEntity({
+        ...req.body,
+        teacherId: req.session.id,
+      });
+      const result = await ClassService.updateClass(req.params.id, entity);
+      return res.status(200).json(result);
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async deleteClass(req, res, next) {
+    try {
+      await ClassService.deleteClass(req.params.id, req.session.id);
+      return res.status(200).json();
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default new ClassController();
--- backend/src/api/class/class.service.js
+++ backend/src/api/class/class.service.js
@@ -1,92 +1,110 @@
-import sequelize from '../../database/models/index.cjs';
-import { ClassDto } from './dtos/index.js';
-import { REQUEST_STATE } from '../../shared/enum/index.js';
-import { EntityNotFoundException, EntityAlreadyExistException, EntityForbiddenUpdateException, EntityForbiddenDeleteException } from '../../exceptions/index.js';
+import sequelize from "../../database/models/index.cjs";
+import { ClassDto } from "./dtos/index.js";
+import { REQUEST_STATE } from "../../shared/enum/index.js";
+import {
+  EntityNotFoundException,
+  EntityAlreadyExistException,
+  EntityForbiddenUpdateException,
+  EntityForbiddenDeleteException,
+} from "../../exceptions/index.js";
 
 const { User, Class, StudentClass } = sequelize;
 class ClassService {
-    async getAllClassesOfTeacher(userId) {
-        const user = await User.findByPk(userId);
-        const data = await Class.findAll({
-            where: {
-                teacherId: userId
-            }
-        });
-        return data.map(x => ClassDto.toDto(x));
+  async getAllClassesById(classId) {
+    const data = await Class.findAll({
+      where: {
+        id: classId,
+      },
+    });
+    return data.map((x) => ClassDto.toDto(x));
+  }
+
+  async getAllClassesOfTeacher(userId) {
+    const data = await Class.findAll({
+      where: {
+        teacherId: userId,
+      },
+    });
+    return data.map((x) => ClassDto.toDto(x));
+  }
+
+  async getAllClassesOfStudent(userId) {
+    const user = await User.findByPk(userId);
+    if (user == null) {
+      throw new EntityNotFoundException("User", userId);
+    }
+    const data = await StudentClass.findAll({
+      where: {
+        studentId: userId,
+        state: REQUEST_STATE.APPROVED,
+      },
+      include: [
+        {
+          model: Class,
+        },
+      ],
+    });
+    return data.map((x) => ClassDto.toDto(x.Class));
+  }
+
+  async createClass(newClass) {
+    const classEntity = await Class.findByPk(newClass.id);
+    if (classEntity != null) {
+      throw new EntityAlreadyExistException("Class", newClass.id);
     }
 
-    async getAllClassesOfStudent(userId) {
-        const user = await User.findByPk(userId);
-        if (user == null) {
-            throw new EntityNotFoundException('User', userId);
-        }
-        const data = await StudentClass.findAll({
-            where: {
-                studentId: userId,
-                state: REQUEST_STATE.APPROVED
-            },
-            include: [{
-                model: Class
-            }]
-        });
-        return data.map(x => ClassDto.toDto(x.Class));
+    const teacher = await User.findByPk(newClass.teacherId);
+    if (teacher == null) {
+      throw new EntityNotFoundException("User", newClass.teacherId);
     }
 
-    async createClass(newClass) {
-        const classEntity = await Class.findByPk(newClass.id);
-        if (classEntity != null) {
-            throw new EntityAlreadyExistException('Class', newClass.id);
-        }
+    return await Class.create(newClass);
+  }
 
-        const teacher = await User.findByPk(newClass.teacherId);
-        if (teacher == null) {
-            throw new EntityNotFoundException('User', newClass.teacherId);
-        }
-
-        return await Class.create(newClass);
+  async updateClass(id, newClass) {
+    const classEntity = await Class.findByPk(id);
+    if (classEntity == null) {
+      throw new EntityNotFoundException("Class", id);
     }
 
-    async updateClass(id, newClass) {
-        const classEntity = await Class.findByPk(id);
-        if (classEntity == null) {
-            throw new EntityNotFoundException('Class', id);
-        }
-
-        const teacher = await User.findByPk(newClass.teacherId);
-        if (teacher == null) {
-            throw new EntityNotFoundException('User', newClass.teacherId);
-        }
-
-        if (teacher.id != classEntity.teacherId) {
-            throw new EntityForbiddenUpdateException('Class', classEntity.id);
-        }
-
-        await Class.update({ ...classEntity, ...newClass }, {
-            where: {
-                id: id
-            }
-        });
-
-        const dto = ClassDto.toDto(await Class.findByPk(id));
-        return dto;
+    const teacher = await User.findByPk(newClass.teacherId);
+    if (teacher == null) {
+      throw new EntityNotFoundException("User", newClass.teacherId);
     }
 
-    async deleteClass(id, teacherId) {
-        const classEntity = await Class.findByPk(id);
-        if (classEntity == null) {
-            throw new EntityNotFoundException('Class', id);
-        }
-        
-        if (classEntity.teacherId != teacherId) {
-            throw new EntityForbiddenDeleteException('Class', id);
-        }
-
-        await Class.destroy({
-            where: {
-                id: id
-            }
-        });
+    if (teacher.id != classEntity.teacherId) {
+      throw new EntityForbiddenUpdateException("Class", classEntity.id);
     }
+
+    await Class.update(
+      { ...classEntity, ...newClass },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    const dto = ClassDto.toDto(await Class.findByPk(id));
+    return dto;
+  }
+
+  async deleteClass(id, teacherId) {
+    const classEntity = await Class.findByPk(id);
+    if (classEntity == null) {
+      throw new EntityNotFoundException("Class", id);
+    }
+
+    if (classEntity.teacherId != teacherId) {
+      throw new EntityForbiddenDeleteException("Class", id);
+    }
+
+    await Class.destroy({
+      where: {
+        id: id,
+      },
+    });
+  }
 }
 
 export default new ClassService();
