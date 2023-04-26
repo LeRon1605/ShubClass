ca98a708c1bd68f5dd6636e787c2e005314923ef
Ron Le <ronle9519@gmail.com>
2023-04-26 12:43:08 +0700

    fix: resolve conflict

--- mobile/app/src/main/java/com/androidexam/shubclassroom/api/RetrofitClient.java
+++ mobile/app/src/main/java/com/androidexam/shubclassroom/api/RetrofitClient.java
@@ -9,15 +9,7 @@ public class RetrofitClient {
     public static Retrofit getRetrofitInstance() {
         if(retrofit == null) {
             retrofit = new Retrofit.Builder()
-<<<<<<< HEAD
                     .baseUrl("https://shubclass-api.onrender.com/api/")
-=======
-<<<<<<< HEAD
-                    .baseUrl("http://localhost:80/api/")
-=======
-                    .baseUrl("https://shubclass-api.onrender.com/api/")
->>>>>>> f6ddea4ea7f887956a60d03bca3dbcc9b629f082
->>>>>>> feature(android)/home_view_student
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
--- mobile/app/src/main/java/com/androidexam/shubclassroom/model/MessageResponse.java
+++ mobile/app/src/main/java/com/androidexam/shubclassroom/model/MessageResponse.java
@@ -2,18 +2,6 @@ package com.androidexam.shubclassroom.model;
 
 public class MessageResponse {
     private String message;
-<<<<<<< HEAD
-=======
-    private String access_token;
-
-    public String getAccess_token() {
-        return access_token;
-    }
-
-    public void setAccess_token(String access_token) {
-        this.access_token = access_token;
-    }
->>>>>>> feature(android)/home_view_student
 
     public void setMessage(String message) {
         this.message = message;
